6bd1cd7fdd0b8a64729286eab282fe4230f0b01a
RedBug312 <redbug312@gmail.com>
2021-03-29 14:13:51 +0800

    Introduce crate argh as getopt
    63 ms, 2506 loc.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,36 @@
 use adams_leaf::utils::json;
 use adams_leaf::utils::config::Config;
 use adams_leaf::cnc::CNC;
-use regex::Regex;
-use std::env;
+use argh::FromArgs;
+
+/// A mixed-criticality and online routing model for TSN network
+#[derive(FromArgs)]
+struct Arguments {
+    #[argh(positional)]
+    algorithm: String,
+    #[argh(positional)]
+    network: String,
+    #[argh(positional)]
+    backgrounds: String,
+    #[argh(positional)]
+    inputs: String,
+    #[argh(positional)]
+    fold: u32,
+    /// path to configuration file
+    #[argh(option, short='c', default="String::from(\"config.example.json\")")]
+    config: String,
+}
 
 fn main() {
-    let (algo_type, topo_file_name, flow_file_name, flow_file_name2, times, config_name) = {
-        let mut args: Vec<String> = env::args().collect();
-        let re = Regex::new(r"--config=([^ ]+)").unwrap();
-        let mut config_name: Option<String> = None;
-        for i in 0..args.len() {
-            if let Some(cap) = re.captures(&args[i]) {
-                config_name = Some(cap[1].to_owned());
-                args.remove(i);
-                break;
-            }
-        }
-        if args.len() == 6 {
-            (
-                args[1].clone(),
-                args[2].clone(),
-                args[3].clone(),
-                args[4].clone(),
-                args[5].parse::<usize>().unwrap(),
-                config_name,
-            )
-        } else {
-            panic!("用法： adams_leaf [algo type] [topo.json] [base_flow.json] [reconf_flow.json] [倍數] (--config=[設定檔])");
-        }
-    };
-    if let Some(config_name) = config_name {
-        println!("{}", config_name);
-        Config::load_file(&config_name).unwrap();
-    }
+    let args: Arguments = argh::from_env();
 
-    let (tsns1, avbs1) = json::load_streams(&flow_file_name, 1);
-    let (tsns2, avbs2) = json::load_streams(&flow_file_name2, times as u32);
-    let network = json::load_network(&topo_file_name);
-    // FIXME 對這個圖作 Yens algo，0->2這條路有時找得到6條，有時只找得到5條
+    let network = json::load_network(&args.network);
+    let (tsns1, avbs1) = json::load_streams(&args.backgrounds, 1);
+    let (tsns2, avbs2) = json::load_streams(&args.inputs, args.fold);
+    Config::load_file(&args.config)
+        .expect("Failed to load config file");
 
-    let mut cnc = CNC::new(&algo_type, network);
+    let mut cnc = CNC::new(&args.algorithm, network);
 
     cnc.add_streams(tsns1, avbs1);
     let elapsed = cnc.configure();
